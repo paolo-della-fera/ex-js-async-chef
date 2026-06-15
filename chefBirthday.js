cdef96af8cdd97747f8f34dd41249d4493cf837f
paolo-della-fera <paolodf2005@gmail.com>
2026-06-15 10:29:14 +0200

    refactor: simplify getChefBirthday function by removing redundant error handling

--- chefBirthday.js
+++ chefBirthday.js
@@ -1,41 +1,19 @@
-async function fetchJson(url) {
-    const response = await fetch(url)
-    const obj = await response.json()
-    return obj
-}
-
 async function getChefBirthday(id) {
 
-    let ricetta 
-    try{
-        ricetta = await fetchJson(`https://dummyjson.com/recipes/${id}`)
-    }catch(error) {
-        throw new Error(`Non posso recuperare la ricetta con l'id:${id}`)
-    }
+    const ricettaId = await fetch(`https://dummyjson.com/recipes/${id}`)
+    const ricetta =  await ricettaId.json()
 
-    if (ricetta.message) {
-        throw new Error(ricetta.message)
-    }
-
-    let chef
-    try{
-        chef = await fetchJson(`https://dummyjson.com/users/${ricetta.userId}`)
-    }catch(error) {
-        throw new Error(`Non posso recuperare la data di nascita dello chef con l'id: ${id}`)
-    }
-
-    if (chef.message) {
-        throw new Error(chef.message)
-    }
-
+    const chefId = await fetch(`https://dummyjson.com/users/${ricetta.userId}`)
+    const chef = await chefId.json()
+    
     return chef.birthDate
 
 }
 
 (async () => {
     try{
-        const date = await getChefBirthday(1)
-        console.log('Data di nascita dello chef:', date)
+        const birthday = await getChefBirthday(1)
+        console.log('Data di nascita dello chef:', birthday)
     }catch(error) {
         console.error(error)
     }finally{
